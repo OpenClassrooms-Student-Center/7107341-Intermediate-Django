140af8eb744cf387338c08e62f8cd7fd35ed86a2
Rafiq Hilali <rafiq@lambertlabs.com>
2021-04-27 23:44:25 +0100

    P3C6S1: added follows users form

--- authentication/models.py
+++ authentication/models.py
@@ -12,6 +12,11 @@ class User(AbstractUser):
     )
     profile_photo = models.ImageField()
     role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+    follows = models.ManyToManyField(
+        'self',
+        limit_choices_to={'role': CREATOR},
+        symmetrical=False
+    )
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
